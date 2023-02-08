2390f640d56c41c70944791e919702a7c968220b
whitepai <whitepai@simbo.com.tw>
2023-02-08 21:28:24 +0800

    fix timezone problem

--- composables/dayjs.js
+++ composables/dayjs.js
@@ -1,14 +1,16 @@
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 dayjs.extend(utc)
+dayjs.extend(timezone)
 
 export const useFormatDateTime = (date) => dayjs(date).format('YYYY-MM-DD HH:mm:ss')
 
 export const useFormatDate = (date) => dayjs(date).format('YYYY-MM-DD')
 
-export const useStartOfDay = () => dayjs().startOf('day').utc().format()
+export const useStartOfDay = () => dayjs.tz(dayjs().startOf('day').format('YYYY-MM-DD HH:mm:ss'), 'Asia/Taipei').format()
 
-export const useEndOfDay = () => dayjs().endOf('day').utc().format()
+export const useEndOfDay = () => dayjs.tz(dayjs().endOf('day').format('YYYY-MM-DD HH:mm:ss'), 'Asia/Taipei').format()
 
 export const useCurrentTime = () => dayjs().utc().format()
 
